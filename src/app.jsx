d36e145696c4e3179a754a8d22a6390b1ca9dc20
beop07 <beop07@naver.com>
2020-10-22 17:40:45 +0900

    Show video list with title;

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
-function App() {
+function App(){
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBs0BXTk3yX_Ug6wSL8yyP3SYR31LaA-Fw", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);// update 될때 마다 호출
   return (
-    <h1>Hello :)</h1>
+    <VideoList key={videos.id} videos={videos}/>
   );
 }
 
